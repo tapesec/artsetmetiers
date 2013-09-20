629e401a1f34abf30055a1697bb2c8fb70093d01
tapesec <lionel.dupouy@gmail.com>
2013-09-20 22:45:30 +0200

    lien icone rss

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -19,7 +19,7 @@
 	<?php echo $this->link('javascript', 'style'); ?>
 	<?php echo $this->link('javascript', 'plugs'); ?>
 	<?php echo $this->link('javascript', 'bootstrap.min'); ?>
-	<link rel="alternate" type="application/rss+xml" title="Le flux rss de cnam-it.fr" href="http://dev.cnam-it.fr/webroot/rss.xml">
+<link rel="alternate" type="application/rss+xml" title="Le flux rss de cnam-it.fr" href="<?php echo '/rss.xml'; ?>">
 	<script>
   		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
  		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -179,7 +179,7 @@
 							</div>
 						
 							<div class="span4">
-								<?php $this->img('design/img/rss2.png', array('alt' => 'icone flux RSS')); ?>
+								<a href="<?php echo '/rss.xml'; ?>"><?php $this->img('design/img/rss2.png', array('alt' => 'icone flux RSS')); ?></a>
 							</div>
 						</div>
 						
