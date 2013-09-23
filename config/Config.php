70ae4fe74298d44010799d69e22a8f4ff7a0c8e0
tapesec <lionel.dupouy@gmail.com>
2013-09-23 22:01:40 +0200

    conflit de merge reglé

--- config/Config.php
+++ config/Config.php
@@ -3,32 +3,18 @@
 * classe de configuration du model de connexion aux bases de données et des routes de l'application
 **/
 class Config{
-
 	/**
-<<<<<<< HEAD
-	* configurez ici l'adresse de votre site internet.
-	**/
-	static $website_adress = 'http://localhost';
-=======
 	* configurez ici l'adresse vers la page de connexion (url appelé en ca de tentative d'acces a une page à privilège.
 	**/
 	static $website_adress = 'http://localhost/artsetmetiers/webroot/auth/connexion';
->>>>>>> dev
-
 
 	static $homeController = 'accueil'; //définissez un controller par défaut pour votre page d'acceuil, ce controlleur devra avoir une méthode par défaut nommé index
 	//par exemple pour $homeController définit à Demo et contenant une méthode index, un site tel que www.monsite.com sera équivalent à www.monsite.com/demo/index
-
 	static $debug_level = 0; // définit le niveau d'information affiché en cas d'erreur dans vos interactions en base de données : 
-	//									0 = aucune info, juste un message générique du type "service momentanément indisponnible (mode production),
-	//									1 = affiche les erreurs sql ou de connexion à PDO.
-
-
+	//0 = aucune info, juste un message générique du type "service momentanément indisponnible (mode production),
+	//1 = affiche les erreurs sql ou de connexion à PDO.
 	static $maxFileSize = 1048576; // (en octets) la taille max par défaut des fichiers à uploader dans vos formulaires
-
 	static $database_name = 'artsetmetiers';  // C'est ici qu'il faut changer défault par le nom de la base souhaité apres l'avoir rajouté au tableau ci-dessous.
-
-
 	/**
 	*configurer ici votre connexion à PDO et votre compte mysql
 	**/
@@ -52,11 +38,10 @@ class Config{
 	* la proprieté statique Config::$acces permet de configurer les droits des utilisateurs pour chaque action de chaque
 	* controlleur, completer le tableau ci dessous pour chaque action necessitant un niveau d'authentification, ne remplissez pas
 	*pour les actions ouvertes au public comme la page d'accueil etc .. Il faudra dans votre table utilisateur créer un champ "statut"
-	*et lui donner des possibilités de valeur identique à ce tableau. Pour l'exemple le controlleur mycontrollerController laisse
+	*et lui donner des possibilités de valeur identique à ce tableau.
+        *Pour l'exemple le controlleur mycontrollerController laisse
 	*tous les utilisateurs ayant le champ statut qui a comme valeur membre acceder à l'action1. 
-	*
-	*
-	*
+	**
 	**/
 	static $access  = array(/*'mycontroller' (Nom du controller sans le suffixe Controller) => array(
 								'action1' => 'membre,
@@ -68,8 +53,6 @@ class Config{
 								'addSubject' => 1,
 								'delReply' => 1,
 								'lockSubject' => 2),
-<<<<<<< HEAD
-=======
 							'message' => array(
 								'index' => 1,
 								'voir' => 1,
@@ -77,9 +60,7 @@ class Config{
 								'getLogin' => 1,
 								'ecrire' => 1,
 								'delete' => 1
-
 							),
->>>>>>> dev
 							'parcours' => array(
 								'edit' => 1),
 							'backoff' => array(
@@ -91,12 +72,8 @@ class Config{
 								'listUsers' => 10,
 								'unite' => 10,
 								'diplome' => 10,
-<<<<<<< HEAD
-								'listCat' => 10));
-=======
 								'listCat' => 10,
 								'fileExplorer' => 10));
->>>>>>> dev
 
 	
 	/**
