7b82f959e13037fbaaab8129dc5867dee3b06124
tapesec <lionel.dupouy@gmail.com>
2013-09-24 18:37:44 +0200

    correction de fautes en page d'accueil

--- views/layouts/include/footer.php
+++ views/layouts/include/footer.php
@@ -1,16 +1,17 @@
 <div class="row-fluid">
 	<div class="span12">
-	<?php $data['pages'] = $this->layoutLoad('Blog', 'page'); ?>
+	<?php $menu = $this->layoutLoad('Blog', 'page'); ?>
+	<?php debug($menu); ?>
 	    <div class="navbar">
 	    	<div class="navbar-inner">
-			<a class="brand" href="#"><h1><?php echo ucfirst($this->request->controller); ?></h1></a>
+		<a class="brand" href="<?php echo BASE_URL; ?>"></a>
 		    	<ul class="nav">
 		    		<li class="divider-vertical"></li>
 				<li class="">
-				<a href="<?php echo BASE_URL.$data['pages'][3]['pag_url']; ?>"><?php echo $data['pages'][3]['pag_name'] ?></a>
+				<a href="<?php echo BASE_URL; ?>"><?php echo  ?></a>
 				</li>
 				<li class="divider-vertical"></li>
-		    		<?php foreach ($data['pages'] as $k => $v): current($v); ?>
+		    		<?php foreach ($var as $k => $v): ?>
 	    				<?php if($v['pag_name'] != 'Accueil') :?>
 	    					<?php if($v['pag_name'] != 'Carte de visite' || isset(Auth::$session)): ?>
 				    			<?php if($v['pag_name'] == ucfirst($this->request->controller)): ?>
