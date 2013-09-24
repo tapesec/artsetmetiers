c0f5dea84e5a0c00fc840d99437cdc9b608f2a76
tapesec <lionel.dupouy@gmail.com>
2013-09-24 21:47:12 +0200

    factorisation de la navigation principale

--- views/layouts/include/mainNav.php
+++ views/layouts/include/mainNav.php
@@ -19,12 +19,6 @@
 					</li>
 					<li class="divider-vertical"></li>
 				<?php endforeach; ?>
-				<?php foreach($menu['admin'] as $k => $v): ?>
-					<li class="">
-					<a href="<?php echo BASE_URL.$v['pag_url']; ?>"><?php echo $v['pag_name'];?></a>
-					</li>
-					<li class="divider-vertical"></li>
-				<?php endforeach; ?>
 				<?php if(!empty(Auth::$session)): ?> 
 					<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">
@@ -35,8 +29,16 @@
 					<li><a href="<?php echo BASE_URL.'/auth/edit'; ?>">Edition profil</a></li>
 					<li><a href="<?php echo BASE_URL.'/auth/logout'; ?>">Déconnection</a></li>
 					</ul>
-					</li>';
+					</li>
+					<li class="divider-vertical"></li>
 				 <?php endif; ?>
+				<?php if(!empty(Auth::$session) && Auth::$session['use_statut'] == 10): ?>
+					<li class="">
+					<a href="<?php echo BASE_URL.'/backoff/index'; ?>">Administration du site</a>
+					</li>
+					<li class="divider-vertical"></li>
+				<?php endif; ?>
+				
 		    	</ul>
 		    </div>
 	      </div>
